be1c497b03bb3bd791698c0e344834ddcd09ed9b
charles-ly <83621372+charles-ly@users.noreply.github.com>
2021-06-07 16:53:43 +1000

    Update stars.js

--- Arcanyx_Type_Sampler/stars.js
+++ Arcanyx_Type_Sampler/stars.js
@@ -4,14 +4,14 @@ class Stars extends Particle { //Creates (Stars) class, inherits code from Parti
     this.angle = random(TWO_PI); //Randomises angle from 
     
   }
-
+//Appearance of 'stars'
   show() {
-    noStroke();
-    fill(255, 231, 0, this.lifetime);
+    noStroke(); //Removes outline
+    fill(255, 231, 0, this.lifetime); //Colour and fade
     push();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.angle);
-    square(0, 0, this.r * 2);
+    translate(this.pos.x, this.pos.y); //Centre of shape
+    rotate(this.angle); //Rotation
+    square(0, 0, this.r * 2); //Draw a square (x, y, width)
     pop();
   }
 }
