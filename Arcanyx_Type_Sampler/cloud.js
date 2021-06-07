821916b199a997c6a0ff1f4352fb3faea280c786
charles-ly <83621372+charles-ly@users.noreply.github.com>
2021-06-07 16:52:50 +1000

    Update cloud.js

--- Arcanyx_Type_Sampler/cloud.js
+++ Arcanyx_Type_Sampler/cloud.js
@@ -1,23 +1,23 @@
-class Cloud {
+class Cloud { //Creates and names a class (Cloud) and acts as a template
 constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D();
-    this.vel.mult(random(0.5, 2));
-    this.acc = createVector(0, 0);
-    this.r = random(290,330);
-    this.lifetime = 255;
-    //this.angle = random(TWO_PI);
+    this.pos = createVector(x, y); //Starting point of particles
+    this.vel = p5.Vector.random2D(); //Makes a 2D vector from a random angle
+    this.vel.mult(random(0.5, 2)); //Controls randomness and spread
+    this.acc = createVector(0, 0); //Direction of movement
+    this.r = random(290,330); //Radius (size image)
+    this.lifetime = 255; //Starting opacity
+
   }
 
-  finished() {
+  finished() { //To delete finished particles (Opacity is <0 )
     return this.lifetime < 0;
   }
 
-  applyForce(force) {
+  applyForce(force) { //Movement
     this.acc.add(force);
   }
 
-  update() {
+  update() { //Animation and movement
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
@@ -25,11 +25,9 @@ constructor(x, y) {
     this.lifetime -= 7;
   }
 
-  show() {
+  show() { //Appearance
     push();
     tint(43, 43, 42, this.lifetime);
-    //translate(this.pos.x, this.pos.y);
-    //rotate(this.angle);
     imageMode(CENTER);
     image(img, this.pos.x, this.pos.y, this.r*1.2, this.r);
     pop();
