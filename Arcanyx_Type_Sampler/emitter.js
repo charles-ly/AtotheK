4a196292e3926d66af191d8f7dc5c6b3392dcb0f
charles-ly <83621372+charles-ly@users.noreply.github.com>
2021-06-07 16:53:07 +1000

    Update emitter.js

--- Arcanyx_Type_Sampler/emitter.js
+++ Arcanyx_Type_Sampler/emitter.js
@@ -1,26 +1,26 @@
-class Emitter {
+class Emitter { //A way of putting a class into a class for easier summoning in sketch
   constructor(x, y) {
     this.position = createVector(x, y);
     this.clouds = [];
   }
 
-  emit(num) {
+  emit(num) { //Number of clouds
     for (let i = 0; i < num; i++) {
       this.clouds.push(new Cloud(windowWidth/2, windowHeight/2));
     }
   }
 
-  applyForce(force) {
+  applyForce(force) { //For direction of movement
     for (let cloud of this.clouds) {
       cloud.applyForce(force);
     }
   }
 
-  update() {
+  update() { //For animation
     for (let cloud of this.clouds) {
       cloud.update();
     }
-
+//Deleting finished clouds
     for (let i = this.clouds.length - 1; i >= 0; i--) {
       if (this.clouds[i].finished()) {
         this.clouds.splice(i, 1);
@@ -28,7 +28,7 @@ class Emitter {
     }
   }
 
-  show() {
+  show() { //For appearance
     for (let cloud of this.clouds) {
       cloud.show();
     }
